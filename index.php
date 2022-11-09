b83c6a1bc907f1356fc5c4f12a5fde971b804d13
Louis Ouellet <louis_ouellet@hotmail.com>
2022-11-09 14:20:57 -0500

    2022-11-09 14:20 - v1.0.1

--- index.php
+++ index.php
@@ -10,14 +10,5 @@ require 'vendor/autoload.php';
 //Initiate phpRouter
 $phpRouter = new phpRouter();
 
-//Add Routes
-$phpRouter->add('/', __DIR__ . '/View/index.php');
-$phpRouter->add('404', __DIR__ . '/View/404.php');
-$phpRouter->add('403', __DIR__ . '/View/403.php');
-$phpRouter->add('500', __DIR__ . '/View/500.php');
-$phpRouter->add('/courses', __DIR__ . '/View/courses.php');
-$phpRouter->add('/views/authors', __DIR__ . '/View/authors.php');
-$phpRouter->add('/about', __DIR__ . '/View/aboutus.php');
-
 //Load Request
 $phpRouter->load();
