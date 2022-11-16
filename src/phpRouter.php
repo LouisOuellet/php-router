c090b94a9c6b8b0a3b6518517363a5216126e3df
Louis Ouellet <louis_ouellet@hotmail.com>
2022-11-16 16:48:44 -0500

    2022-11-16 16:48 - v1.3.5

--- src/phpRouter.php
+++ src/phpRouter.php
@@ -139,7 +139,7 @@ class phpRouter {
 
   protected function add($route, $view, $template = null, $label = null, $public = true, $error = null){
     if(is_file($view) && (is_file($template) || $template == null)){
-      $this->Routes[$route] = [ "view" => $view, "template" => $template, "public" => $public, "error" => $error ];
+      $this->Routes[$route] = [ "view" => $view, "template" => $template, "label" => $label, "public" => $public, "error" => $error ];
       return true;
     }
     return false;
