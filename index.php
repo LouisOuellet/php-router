27f2a861d29aedba11b4740117db859376f4a295
Louis Ouellet <louis_ouellet@hotmail.com>
2023-02-07 13:52:27 -0500

    2023-02-07 13:52 - v1.3.24

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
 <?php
-//Configure Cookie Scope
-session_set_cookie_params(['samesite' => 'None']);
 
-//Initiate Session
-session_start();
+define('ROUTER_ROOT',__DIR__);
 
-//Import API class into the global namespace
-//These must be at the top of your script, not inside a function
-use LaswitchTech\phpRouter\phpRouter;
-
-//Load Composer's autoloader
-require 'vendor/autoload.php';
-
-//Defining Routes
-define('ROUTER_ROUTES',[
-  "404" => ["view" => "View/404.php", "label" => "404 - Not Found"],
-  "/" => ["view" => "View/index.php", "template" => "Template/index.php", "public" => false, "error" => "/signin"],
-  "/signin" => ["view" => "View/signin.php"],
-  "/info" => ["view" => "View/info.php"],
-  "/install" => ["view" => "View/install.php"],
-]);
-
-//Defining Requirements
-define("ROUTER_REQUIREMENTS", ["APACHE" => ["mod_rewrite"]]);
-
-//Initiate phpRouter
-$phpRouter = new phpRouter();
-
-//Render Request
-$phpRouter->render();
+require __DIR__ . '/webroot/index.php';
--- src/phpRouter.php
+++ src/phpRouter.php
@@ -20,9 +20,25 @@ class phpRouter {
 
   public function __construct(){
 
-    // Configuring Router
+    // Identifying Project Root
+    if($this->Path == null){
+      if(defined('ROUTER_ROOT')){
+        $this->Path = ROUTER_ROOT;
+      } else {
+        $this->Path = dirname(__DIR__);
+      }
+    }
+    if(!defined('ROUTER_ROOT')){
+      define('ROUTER_ROOT',$this->Path);
+    }
+
+    // Check Requirements
     $this->checkRequirements();
-    $this->genHTAccess();
+
+    // Setup Webroot
+    $this->genWebroot();
+
+    // Configuring Router
     if($this->URI == null){ $this->URI = $_SERVER['REQUEST_URI']; }
     if($this->URI == ''){ $this->URI = '/'; }
     $this->URI = explode('?',$this->URI)[0];
@@ -86,30 +102,171 @@ class phpRouter {
     return $make;
   }
 
+  protected function scandir($directory, $filter = "ANY"){
+    if(!str_starts_with($directory,'/')){ $directory = '/' . $directory; }
+    $path = $this->Path . $directory;
+    if(!str_ends_with($path,'/')){ $path .= '/'; }
+    $files = [];
+    foreach(scandir($path) as $file){
+      if($filter){
+        switch(strtoupper($filter)){
+          case"DIRECTORY":
+          case"DIRECTORIES":
+          case"DIR":
+            if(is_dir($path.$file) && !in_array($file,['.','..'])){
+              $files[] = $file;
+            }
+            break;
+          case"FILES":
+          case"FILE":
+            if(is_file($path.$file) && !in_array($file,['.DS_Store'])){
+              $files[] = $file;
+            }
+            break;
+          case"ALL":
+          case"ANY":
+            if((is_file($path.$file) && !in_array($file,['.DS_Store'])) || (is_dir($path.$file) && !in_array($file,['.','..']))){
+              $files[] = $file;
+            }
+            break;
+        }
+      } else {
+        $files[] = $file;
+      }
+    }
+    return $files;
+  }
+
+  protected function getIndex(){
+    $index = '';
+    $index .= '<?php' . PHP_EOL;
+    $index .= PHP_EOL;
+    $index .= '//Initiate Session' . PHP_EOL;
+    $index .= 'session_start();' . PHP_EOL;
+    $index .= PHP_EOL;
+    $index .= '//Import phpRouter class into the global namespace' . PHP_EOL;
+    $index .= 'use LaswitchTech\phpRouter\phpRouter;' . PHP_EOL;
+    $index .= PHP_EOL;
+    $index .= 'if(!defined("ROUTER_ROOT")){' . PHP_EOL;
+    $index .= '  define("ROUTER_ROOT",dirname(__DIR__));' . PHP_EOL;
+    $index .= '}' . PHP_EOL;
+    $index .= PHP_EOL;
+    $index .= '//Load Composer\'s autoloader' . PHP_EOL;
+    $index .= 'require ROUTER_ROOT . "/vendor/autoload.php";' . PHP_EOL;
+    $index .= PHP_EOL;
+    $index .= '//Initiate phpRouter' . PHP_EOL;
+    $index .= '$phpRouter = new phpRouter();' . PHP_EOL;
+    $index .= PHP_EOL;
+    $index .= '//Render Request' . PHP_EOL;
+    $index .= '$phpRouter->render();' . PHP_EOL;
+
+    return $index;
+
+    // //Defining Routes
+    // define("ROUTER_ROUTES",[
+    //   "404" => ["view" => "View/404.php", "label" => "404 - Not Found"],
+    //   "/" => ["view" => "View/index.php", "template" => "Template/index.php", "public" => false, "error" => "/signin"],
+    //   "/signin" => ["view" => "View/signin.php"],
+    //   "/info" => ["view" => "View/info.php"],
+    //   "/install" => ["view" => "View/install.php"],
+    // ]);
+    //
+    // //Defining Requirements
+    // define("ROUTER_REQUIREMENTS", ["APACHE" => ["mod_rewrite"]]);
+  }
+
+  protected function genIndex($webroot){
+    $file = $webroot . '/index.php';
+    if(!is_file($file)){
+      file_put_contents($file, $this->getIndex());
+    }
+  }
+
+  protected function genWebroot(){
+    $webroot = $this->mkdir('webroot');
+    foreach($this->scandir('dist','directory') as $directory){
+      if(!str_starts_with($directory,'/')){ $directory = '/' . $directory; }
+      $link = $webroot.$directory;
+      $target = $this->Path.'/dist'.$directory;
+      if(is_dir($target) && !is_dir($link) && !is_file($link)){
+        symlink($target, $link);
+      }
+    }
+    $this->genHTAccess();
+    $this->genIndex();
+  }
+
   protected function genHTAccess(){
-    if($this->Path == null){ $this->Path = dirname(\Composer\Factory::getComposerFile()); }
     if(!is_file($this->Path . '/.htaccess')){
-      if(is_file($this->Path . '/View/404.php')){
-        $htaccess = 'ErrorDocument 404 "/View/404.php"\n';
+      $htaccess = '';
+      $file = $this->Path . '/View/500.php';
+      if(is_file($file)){
+        $htaccess .= 'ErrorDocument 500 "' . $file . '"';
+        $htaccess .= PHP_EOL;
       }
-      if(is_file($this->Path . '/View/403.php')){
-        $htaccess = 'ErrorDocument 403 "/View/403.php"\n';
+      $file = $this->Path . '/View/404.php';
+      if(is_file($file)){
+        $htaccess .= 'ErrorDocument 404 "' . $file . '"';
+        $htaccess .= PHP_EOL;
       }
-      $htaccess = "Options All -Indexes\n";
-      $htaccess .= "\n";
-      $htaccess .= "<IfModule mod_rewrite.c>\n";
-      $htaccess .= "  RewriteEngine On\n";
-      $htaccess .= "  RewriteBase /\n";
-      $htaccess .= "  RewriteCond %{REQUEST_FILENAME} !-d\n";
-      $htaccess .= "  RewriteCond %{REQUEST_FILENAME} !-f\n";
-      $htaccess .= "  RewriteRule ^(.+)$ index.php [QSA,L]\n";
-      $htaccess .= "  RewriteRule ^config/.*$ - [F,L]\n";
-      $htaccess .= "  RewriteRule ^tmp/.*$ - [F,L]\n";
-      $htaccess .= "  RewriteRule ^data/.*$ - [F,L]\n";
-      $htaccess .= "  RewriteRule ^cli - [F,L]\n";
-      $htaccess .= "  RewriteRule ^.htaccess - [F,L]\n";
-      $htaccess .= "</IfModule>\n";
+      $file = $this->Path . '/View/403.php';
+      if(is_file($file)){
+        $htaccess .= 'ErrorDocument 403 "' . $file . '"';
+        $htaccess .= PHP_EOL;
+      }
+      $htaccess .= "Options All -Indexes" . PHP_EOL;
+      $htaccess .= PHP_EOL;
+      $htaccess .= "<IfModule mod_headers.c>" . PHP_EOL;
+      $htaccess .= "  RequestHeader unset Proxy" . PHP_EOL;
+      $htaccess .= "</IfModule>" . PHP_EOL;
+      $htaccess .= PHP_EOL;
+      $htaccess .= "<IfModule mod_rewrite.c>" . PHP_EOL;
+      $htaccess .= "  RewriteEngine on" . PHP_EOL;
+      $htaccess .= "  RewriteRule ^(\.well-known/.*)$ $1 [L]" . PHP_EOL;
+      $htaccess .= "  RewriteRule ^$ webroot/ [L]" . PHP_EOL;
+      $htaccess .= "  RewriteRule (.*) webroot/$1 [L]" . PHP_EOL;
+      $htaccess .= "</IfModule>" . PHP_EOL;
+
       file_put_contents($this->Path . '/.htaccess', $htaccess);
+    }
+
+    if(!is_file($this->Path . '/webroot/.htaccess')){
+      $htaccess = '';
+      $file = $this->Path . '/View/500.php';
+      if(is_file($file)){
+        $htaccess .= 'ErrorDocument 500 "' . $file . '"';
+        $htaccess .= PHP_EOL;
+      }
+      $file = $this->Path . '/View/404.php';
+      if(is_file($file)){
+        $htaccess .= 'ErrorDocument 404 "' . $file . '"';
+        $htaccess .= PHP_EOL;
+      }
+      $file = $this->Path . '/View/403.php';
+      if(is_file($file)){
+        $htaccess .= 'ErrorDocument 403 "' . $file . '"';
+        $htaccess .= PHP_EOL;
+      }
+      $htaccess .= "Options All -Indexes" . PHP_EOL;
+      $htaccess .= PHP_EOL;
+      $htaccess .= "<IfModule mod_headers.c>" . PHP_EOL;
+      $htaccess .= "  RequestHeader unset Proxy" . PHP_EOL;
+      $htaccess .= "</IfModule>\n";
+      $htaccess .= PHP_EOL;
+      $htaccess .= "<IfModule mod_rewrite.c>" . PHP_EOL;
+      $htaccess .= "  RewriteEngine On" . PHP_EOL;
+      $htaccess .= "  RewriteBase /" . PHP_EOL;
+      $htaccess .= "  RewriteCond %{REQUEST_FILENAME} !-d" . PHP_EOL;
+      $htaccess .= "  RewriteCond %{REQUEST_FILENAME} !-f" . PHP_EOL;
+      $htaccess .= "  RewriteRule ^(.+)$ index.php [QSA,L]" . PHP_EOL;
+      $htaccess .= "  RewriteRule ^config/.*$ - [F,L]" . PHP_EOL;
+      $htaccess .= "  RewriteRule ^tmp/.*$ - [F,L]" . PHP_EOL;
+      $htaccess .= "  RewriteRule ^data/.*$ - [F,L]" . PHP_EOL;
+      $htaccess .= "  RewriteRule ^cli - [F,L]" . PHP_EOL;
+      $htaccess .= "  RewriteRule ^.htaccess - [F,L]" . PHP_EOL;
+      $htaccess .= "</IfModule>" . PHP_EOL;
+
+      file_put_contents($this->Path . '/webroot/.htaccess', $htaccess);
     }
   }
 
@@ -134,7 +291,7 @@ class phpRouter {
                       $this->sendOutput('This application requires the '.strtoupper($server).' module: '.strtolower($module).'.', array('HTTP/1.1 500 Internal Error'));
                     }
                   } else {
-                    $this->sendOutput('This application requires a '.strtoupper($requirement).' server.', array('HTTP/1.1 500 Internal Error'));
+                    // $this->sendOutput('This application requires a '.strtoupper($server).' server with ' . $module . ' module enabled.', array('HTTP/1.1 500 Internal Error'));
                   }
                   break;
                 case"PHP":
@@ -203,10 +360,22 @@ class phpRouter {
   }
 
   protected function add($route, $view, $template = null, $label = null, $public = true, $error = null){
+    if($view != null){
+      $view = ROUTER_ROOT . '/' . $view;
+    }
+    if($template != null){
+      $template = ROUTER_ROOT . '/' . $template;
+    }
     if($view != null && is_file($view) && ($template == null || is_file($template))){
       $this->Routes[$route] = [ "view" => $view, "template" => $template, "label" => $label, "public" => $public, "error" => $error ];
       return true;
     }
+    echo "Route: " . json_encode($route,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL;
+    echo "View: " . json_encode($view,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL;
+    echo "Template: " . json_encode($template,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL;
+    echo "Test: " . json_encode($view != null,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL;
+    echo "Test: " . json_encode(is_file($view) != null,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL;
+    echo "Test: " . json_encode(($template == null || is_file($template)) != null,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL;
     return false;
   }
 
@@ -237,14 +406,18 @@ class phpRouter {
       return true;
     } else {
       $this->Route = '404';
-      $this->View = $this->Routes['404']['view'];
-      $this->Template = $this->Routes['404']['template'];
-      $this->Label = $this->Routes['404']['label'];
+      if(isset($this->Routes['404']['view'])){ $this->View = $this->Routes['404']['view']; }
+      else { $this->View = null; }
+      if(isset($this->Routes['404']['template'])){ $this->Template = $this->Routes['404']['template']; }
+      else { $this->Template = null; }
+      if(isset($this->Routes['404']['label'])){ $this->Label = $this->Routes['404']['label']; }
+      else { $this->Label = null; }
     }
     return false;
   }
 
   public function render(){
+    // echo json_encode($this->Routes,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     if(!isset($this->Routes[$this->Route]) || $this->Route == '404'){ http_response_code(404); }
     if($this->Template != null){ require $this->Template; return $this->Template; }
     if($this->View != null){ require $this->View; return $this->View; }
